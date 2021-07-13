1c5ab0197e1d43719953fa977337695219a2989c
sewanugideon <87360078+sewanugideon@users.noreply.github.com>
2021-07-13 10:25:51 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // string course;          
+           // string input;
+
+           while(true){
+
+            Console.WriteLine("Please Enter whether you're Part-Time or Full-Time:");  
+            string input = Console.ReadLine();
+            
+            if (input.ToLower().Contains("full"))
+            {
+              Console.WriteLine("Enter your course of study in Full-Time:");
+              string course = Console.ReadLine();
+              Console.WriteLine(course+" is a good course");
+              break;
+            }
+
+            if(input.Substring(0,4).ToLower().Contains("part"))
+            {
+              Console.WriteLine("How many credit are you taking ?");
+              int partTimeCredit = Convert.ToInt32(Console.ReadLine());
+                if(partTimeCredit > 6)
+                {
+                  Console.WriteLine("Too much unit for a part-time student.");
+                   continue;
+                }
+                else
+                {
+                  Console.WriteLine("Proceed to the screen.");
+                   break;
+                }
+            }
+
+           }
+        }
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace csharp2
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+         while(true)
+            {
+          
+                
+             //float a,b,c,d,e,f;
+
+                Console.WriteLine("Enter A:");
+                float a = Convert.ToSingle( Console.ReadLine() );
+
+                Console.WriteLine("Enter B:");
+                float b = Convert.ToSingle( Console.ReadLine() );
+
+                Console.WriteLine("Enter C:");
+                float c = Convert.ToSingle( Console.ReadLine() );
+
+                Console.WriteLine("Enter D:");
+                float d = Convert.ToSingle( Console.ReadLine() );
+
+                Console.WriteLine("Enter E:");
+                float e = Convert.ToSingle( Console.ReadLine() );
+
+                Console.WriteLine("Enter F:");
+                float f = Convert.ToSingle( Console.ReadLine() );
+
+                float determinant = (a*d) - (b*c);
+                float changeInX   = (e*d) - (b*f);
+                float changeInY   = (a*f) - (e*c);
+
+                if(determinant == 0)
+                {
+                    Console.WriteLine("The equation has no solution.");
+                    continue;
+                }
+                else
+                {
+                    float x = changeInX/determinant;
+                    float y = changeInY/determinant;
+
+                    Console.WriteLine("The value Of \n X is {0} \n Y is {1} ", x, y);
+                }
+                break;
+            }
+        }
+    }
+}
